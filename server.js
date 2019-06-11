307ed9bcf0fccdab0e95e4c6bd9b62e2a38a0340
Lucas Henrique Duarte Pereira <1178557@sga.pucminas.br>
2019-06-11 20:08:25 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+// server.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./app/routes');
+const app = express();
+var compression = require('compression');
+
+const port = process.env.PORT || 8000;
+
+app.use(compression());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('./'));
+
+routes(app);
+app.listen(port, () => {
+    console.log('We are live on ' + port);
+});
